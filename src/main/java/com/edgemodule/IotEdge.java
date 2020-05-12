6806d91ac8e83679596ca9441397455c916dd52b
jmbrunskill <james.brunskill@gmail.com>
2020-05-12 22:22:02 +1200

    Updated outputs to stderr so they are displayed in iotedge logs

--- src/main/java/com/edgemodule/IotEdge.java
+++ src/main/java/com/edgemodule/IotEdge.java
@@ -1,35 +1,20 @@
 package com.edgemodule;
-import java.io.StringReader;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-
-import com.microsoft.azure.sdk.iot.device.DeviceTwin.Pair;
-import com.microsoft.azure.sdk.iot.device.DeviceTwin.Property;
-import com.microsoft.azure.sdk.iot.device.DeviceTwin.TwinPropertyCallBack;
 import com.microsoft.azure.sdk.iot.device.*;
 import com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus;
 
 
 public class IotEdge {
     private static MessageCallbackMqtt msgCallback = new MessageCallbackMqtt();
-    private static EventCallback eventCallback = new EventCallback();
     private static final String INPUT_NAME = "input1";
     private static final String OUTPUT_NAME = "output1";
-    private static final String TEMP_THRESHOLD = "TemperatureThreshold";
-    private static AtomicLong tempThreshold = new AtomicLong(25);
 
     protected static class EventCallback implements IotHubEventCallback {
         @Override
         public void execute(IotHubStatusCode status, Object context) {
             if (context instanceof Message) {
-                System.out.println("Send message with status: " + status.name());
+                System.err.println("Send message with status: " + status.name());
             } else {
-                System.out.println("Invalid context passed");
+                System.err.println("Invalid context passed");
             }
         }
     }
@@ -42,17 +27,10 @@ public class IotEdge {
         public IotHubMessageResult execute(Message msg, Object context) {
             this.counter += 1;
             String msgString = new String(msg.getBytes(), Message.DEFAULT_IOTHUB_MESSAGE_CHARSET);
-            System.out.println(String.format("Received message %d: %s", this.counter, msgString));
+            System.err.println(String.format("Received message %d: %s", this.counter, msgString));
             return IotHubMessageResult.COMPLETE;
         }
       
-    }
-
-    protected static class DeviceTwinStatusCallBack implements IotHubEventCallback {
-        @Override
-        public void execute(IotHubStatusCode status, Object context) {
-            System.out.println("IoT Hub responded to device twin operation with status " + status.name());
-        }
     }
 
     protected static class ConnectionStatusChangeCallback implements IotHubConnectionStatusChangeCallback {
@@ -64,17 +42,17 @@ public class IotEdge {
             String statusStr = "Connection Status: %s";
             switch (status) {
                 case CONNECTED:
-                    System.out.println(String.format(statusStr, "Connected"));
+                    System.err.println(String.format(statusStr, "Connected"));
                     break;
                 case DISCONNECTED:
-                    System.out.println(String.format(statusStr, "Disconnected"));
+                    System.err.println(String.format(statusStr, "Disconnected"));
                     if (throwable != null) {
                         throwable.printStackTrace();
                     }
                     System.exit(1);
                     break;
                 case DISCONNECTED_RETRYING:
-                    System.out.println(String.format(statusStr, "Retrying"));
+                    System.err.println(String.format(statusStr, "Retrying"));
                     break;
                 default:
                     break;
@@ -84,10 +62,10 @@ public class IotEdge {
 
     public static void start(){
         try {
-            IotHubClientProtocol protocol = IotHubClientProtocol.MQTT;
-            System.out.println("Start to create client with MQTT protocol");
+            IotHubClientProtocol protocol = IotHubClientProtocol.AMQPS;
+            System.err.println("Start to create client with AMQPS protocol");
             ModuleClient client = ModuleClient.createFromEnvironment(protocol);
-            System.out.println("Client created");
+            System.err.println("Client created");
             client.setMessageCallback(IotEdge.INPUT_NAME, msgCallback, client);
             client.registerConnectionStatusChangeCallback(new ConnectionStatusChangeCallback(), null);
             client.open();
